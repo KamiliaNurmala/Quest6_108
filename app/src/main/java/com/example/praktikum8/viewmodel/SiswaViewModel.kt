339a5fd735cc1fde270ecbb5337f0512d692f927
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-11-20 15:40:38 +0700

    Menambahkan import di SiswaViewModel.kt

---
+++ app/src/main/java/com/example/praktikum8/viewmodel/SiswaViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.praktikum8.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.praktikum8.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+
+class SiswaViewModel {
+}
