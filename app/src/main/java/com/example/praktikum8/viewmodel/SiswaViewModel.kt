2dc6a694e0cdaf4675bee133487d8835e35149a2
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-11-20 15:46:52 +0700

    Menambahkan functcion setSiswa

--- app/src/main/java/com/example/praktikum8/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/praktikum8/viewmodel/SiswaViewModel.kt
@@ -7,10 +7,13 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class SiswaViewModel : viewModel() {
+class SiswaViewModel : ViewModel() {
     private val _statusUI = MutableStateFlow(Siswa())
     val statusUI : StateFlow<Siswa> = _statusUI.asStateFlow()
 
-
+    fun setSiswa(ls:MutableList<String>){
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(nama=ls[0], gender=ls[1], alamat=ls[2])
+        }
     }
 }
