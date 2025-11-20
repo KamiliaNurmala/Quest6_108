5f9fb64bfc7d50ddf54386a0701890d2bcd93f95
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-11-20 15:43:26 +0700

    Menambahkan mutablestateflow dan stateflow

--- app/src/main/java/com/example/praktikum8/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/praktikum8/viewmodel/SiswaViewModel.kt
@@ -4,7 +4,13 @@ import androidx.lifecycle.ViewModel
 import com.example.praktikum8.model.Siswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class SiswaViewModel {
+class SiswaViewModel : viewModel() {
+    private val _statusUI = MutableStateFlow(Siswa())
+    val statusUI : StateFlow<Siswa> = _statusUI.asStateFlow()
+
+
+    }
 }
